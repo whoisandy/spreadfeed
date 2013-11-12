345e7b207696196e24f1f1353cf47c748b16413e
Bhargav Anand <rjn143@gmail.com>
2013-11-12 14:06:58 -0500

    options issue fixed. can now take both key and url

--- js/spreadfeed.js
+++ js/spreadfeed.js
@@ -18,38 +18,52 @@ if( typeof Object.create !== 'function' ) {
       self.$elem = $(elem);
 
       if( typeof options === 'string') {
-        self.key = options;
+        if(/^https:\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/.test(options)){
+          self.url = options;
+          self.options = $.extend({}, $.fn.spreadfeed.options, {url: options});
+        } else {
+          self.key = options;
+          self.options = $.extend({}, $.fn.spreadfeed.options, {key: options});
+        }
       } else if (typeof options === 'object') {
-        if (options.key == null){
-          console.log('Please provide a spreadsheet key');
+        if (options.url === undefined && options.key === undefined){
+          self.$elem.html('<p>Please provide a spreadsheet url or key.</p>');
+          console.log('Please provide a spreadsheet url or key.');
           return;
         } else {
+          self.url = options.url;
           self.key = options.key;
           self.sheet = options.sheet;
-          self.url = 'https://spreadsheets.google.com/feeds/list/'+self.key+'/'+self.sheet+'/public/basic?alt=json&callback=?';
         }
         self.options = $.extend({}, $.fn.spreadfeed.options, options);
       } else {
-        console.log('Please provide a key');
+        console.log('Please provide a valid spreadsheet key or url');
         return;
       }
 
-      self.url = ( self.sheet == null ) ? this.buildSingleUrl(self.key) : this.buildMultipleUrl(self.key, self.sheet);
+      this.url = self.url ===  undefined ? this.buildFromKeySheet(self.options.key, self.options.sheet) : this.buildFromUrl(self.options.url);
 
-      self.showTable();
+      this.showTable();
 
     },
 
-    buildSingleUrl: function(key){
-      return 'https://spreadsheets.google.com/feeds/list/' + key + '/1/public/basic?alt=json&callback=?';
+    buildFromKeySheet: function(key, sheet){
+      return "https://spreadsheets.google.com/feeds/list/" + key + "/" + sheet +
+             "/public/basic?alt=json";
     },
 
-    buildMultipleUrl: function(key, sheet){
-      return 'https://spreadsheets.google.com/feeds/list/' + key + '/' + sheet + '/public/basic?alt=json&callback=?';
+    buildFromUrl: function(url){
+      var keyReg = /(.*key=)(.*)(&output.*)/;
+          key = url.match(keyReg);
+
+      if(key === undefined || key === null){
+        return false;
+      }
+
+      return this.buildFromKeySheet(key[2], this.options.sheet);
     },
 
     fetch: function(url){
-      var self = this;
       return $.getJSON(url);
     },
 
@@ -59,8 +73,12 @@ if( typeof Object.create !== 'function' ) {
 
     sanitize: function(objs){
       var results = $.map(objs.feed.entry, function(result){
-        var ta = result.content.$t.toString().trim().split(', ');
+        var ta = result.content.$t.toString().trim().split(", ");
         var td = $.map(ta, function(v,i){
+          if( v.split(':')[0] === undefined || v.split(':')[1] === undefined ){
+            console.log('Cell values cannot have comma separated values. Please read the docs for more information');
+            return;
+          }
           return '"'+v.split(':')[0].trim() + '":"' + v.split(':')[1].trim() + '"';
         });
         td.push('"'+'title'+'":"'+result.title.$t+'"');
@@ -107,7 +125,7 @@ if( typeof Object.create !== 'function' ) {
       for(i=0; i < self.getRowCount(data); i++){
         var row = $('<tr></tr>');
 
-        for(j=0; j<self.getColCount(data); j++){
+        for(j=0; j < self.getColCount(data); j++){
           var col = $('<td></td>');
           data[i][list[j]] == undefined ? col.append('NA') : col.append(data[i][list[j]]);
           row.append(col);
@@ -136,13 +154,12 @@ if( typeof Object.create !== 'function' ) {
       var self = this;
       self.$elem.html('<p>Loading...</p>');
       $.when(self.fetch(self.url)).done(function(results){
-        console.log(results);
         self.buildTable(self.sanitize(results), self.$elem);
       }).fail(function(){
         self.$elem.html('<p>Unable to fetch data. Please try refreshing.</p>');
         console.log('Unable to fetch data. Please try refreshing.');
       });
-    },
+    }
   };
 
   $.fn.spreadfeed = function( options ){
@@ -153,7 +170,8 @@ if( typeof Object.create !== 'function' ) {
   };
 
   $.fn.spreadfeed.options = {
-    key: '',
+    url: null,
+    key: null,
     sheet: '1'
   };
 
